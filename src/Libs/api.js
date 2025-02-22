367288241153aab8adbdfcab5fd36f527d5af226
kevinpatel18 <patelkevin18102000@gmail.com>
2025-02-22 15:23:12 +0530

    UPdate the backend Link

--- src/Libs/api.js
+++ src/Libs/api.js
@@ -1,7 +1,7 @@
 // const baseUrl =
 // "https://fd8a6942-2a37-4632-8c05-bd8874f009d9.e1-us-east-azure.choreoapps.dev/";
-const baseUrl = "http://localhost:8080/";
-// const baseUrl = "https://pllayer-backend-68514470993.us-central1.run.app/";
+// const baseUrl = "http://localhost:8080/";
+const baseUrl = "https://pllayer-backend-68514470993.us-central1.run.app/";
 
 export const getAllSportDetails = async () => {
   const data = await fetch(`${baseUrl}sports`, {
